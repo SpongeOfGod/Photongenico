5ebb3958bfbe4391b0c1a837c64ee63c5ed1e330
Accun02 <fhermopelufo@uade.edu.ar>
2025-11-03 18:45:49 -0300

    ad

--- Assets/ladderboard.cs
+++ Assets/ladderboard.cs
@@ -20,7 +20,7 @@ public class ladderboard : MonoBehaviourPunCallbacks
     {
 
       
-        Instantiate(ScorePrefabs[PhotonNetwork.CurrentRoom.PlayerCount - 1], transform.parent);
+        Instantiate(ScorePrefabs[PhotonNetwork.CurrentRoom.PlayerCount - 1], this.transform.parent);
 
 
 
